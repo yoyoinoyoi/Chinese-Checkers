d63e262a69e36c89e0dcc0430b2376812f45982e
yoyoinoyoi <y.mar.97p25@gmail.com>
2022-09-14 11:47:57 +0900

    settings.py の説明文を追加

--- settings.py
+++ settings.py
@@ -1,11 +1,19 @@
 # Settings
 
-# 
+# 実装済み
 # Selfplay: ["selfplay"]
 # Alphabeta: ["alphabeta", depth<int>, val_func<str>]
+    # depth: 探索の深さ(何手先を読むか)
+    # val_func: 評価関数(現在はfunc1 のみ)
 # MCTS: ["mcts", thinking_time<float>, val<str>]
+    # thinking_time: 思考時間[s]
+    # val: 評価指標(現在はUCB1 のみ. "-"で指定してください)
+
+# 未実装
 # RAVE: ["RAVE", thinging_time<float>, val<str>]
+
 # example
 # players = [["selfplay"], ["alphabeta", 4, "func1"]]
+# players の長さは2, 3, 6のいずれかで指定してください
 
 players = [["alphabeta", 4, "func1"], ["mcts", 10.0, "-"]]
